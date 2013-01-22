446046525c1d635a17c75a0383e8bbef0c5af120
Ante <akegalj@gmail.com>
2013-01-22 22:52:12 +0100

    Add classes chapter

---
+++ classes/example2.cpp
@@ -0,0 +1,29 @@
+// ecample: one slass, two objects
+#include<iostream>
+using namespace std;
+
+class CRectangle {
+    int x, y;
+  public:
+    void set_values(int, int);
+    int area()
+    {
+      return x * y;
+    }
+};
+
+void CRectangle::set_values(int a, int b) {
+  x = a;
+  y = b;
+}
+
+int main() {
+  CRectangle rect, rectb;
+  rect.set_values(3,4);
+  rectb.set_values(5,6);
+  cout << "rect area: " << rect.area() << endl;
+  cout << "rectb area: " << rectb.area() << endl;
+
+  return 0;
+}
+
