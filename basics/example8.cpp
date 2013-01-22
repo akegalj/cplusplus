9bef68ed4b5c353649ec5d21c7254b811f12b371
Ante <akegalj@gmail.com>
2013-01-22 11:20:00 +0100

    Add basics chapter

---
+++ basics/example8.cpp
@@ -0,0 +1,21 @@
+// assignment operator
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int a, b;   // a:?, b:?
+  a = 10;
+  b = 4;
+  a = b;
+  b = 7;
+
+
+  cout << "a:";
+  cout << a;
+  cout << " b:";
+  cout << b;
+
+  return 0;
+}
