f08078e991c0010e2982a4e3040ea618edfcb303
akegalj <akegalj@gmail.com>
2015-04-09 17:40:16 +0200

    Add proper initialization of cond variables. Call srand only once.

--- rltest/dining_philosophers/main.cpp
+++ rltest/dining_philosophers/main.cpp
@@ -2,27 +2,25 @@
 //
 //   compiled with g++ -lpthread
 //
-// Bugs: Deadlock when philosopher 0 is eating, and philosopher 1 (next to him)
-//       wants to take forks. Then, philosopher 1 has taken mutex, and wont 
-//       give it back untill philosopher 0 puts his forks down. Philosopher 0 
-//       can't put his forks down because of taken mutex, so we have a deadlock.
-//
-//       Problem is Philosopher::take_fork, call for pthread_cond_wait(a,b)
-//       isn't releasing mutex b. Can't figure out why :/
-//       
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 #include "constants.h"
 #include "philosopher.h"
 
 
 int main() {
+  srand(time(NULL));
+
   pthread_cond_t cond[N];
   pthread_mutex_t mon;
   sem_t var_i;
   bool fork[N];
-  for(int i=0; i<N; i++)
+  for(int i=0; i<N; i++) {
     fork[i] = true;
+    pthread_cond_init(&cond[i], NULL);
+  }
 
   pthread_mutex_init(&mon, NULL);
   pthread_cond_init(cond, NULL);
